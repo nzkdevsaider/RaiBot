5b5bbffbbb67468f7ac3a813be549b8d4d2cba65
Raiwex <60488424+Raiwex@users.noreply.github.com>
2020-06-01 14:24:08 +0200

    Loop command

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -5,22 +5,29 @@ const { Utils } = require('erela.js');
 
 module.exports = {
     config: {
-    name: 'pause',
-    description: 'Pauses the music',
-    usage: `${prefix}pause`,
+    name: 'loop',
+    description: 'Makes the song play in loop',
+    usage: `${prefix}loop`,
     category: 'music',
-    access: 'everyone'
+    access: 'everyone',
+    aliases: ['repeat']
 },
 
 run: async (client, message, args) => {
-    
+
     const { channel } = message.member.voice;
     const player = client.music.players.get(message.guild.id);
+    let previousState = player.trackRepeat;
 
     if (channel && channel.id !== player.voiceChannel.id) return message.reply('You need to be in a voice channel!');
     if (!player) return message.channel.send('No song/s currently playing in this server.');
 
-    player.pause(player.playing);
-    return message.channel.send(`Successfully **${player.playing ? 'resumed' : 'paused'}** the music.`)
+    player.setTrackRepeat(!previousState);
+    if(!previousState) {
+    message.channel.send('Loop Mode **ON**.')
+    } else {
+    message.channel.send('Loop Mode **OFF**.')
+    }
+
 }
 }
