c9a04ac14ac9ad970445698e9dc4b9c80619d529
Raiwex <60488424+Raiwex@users.noreply.github.com>
2020-06-04 00:28:34 +0200

    Rework poll command

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -5,7 +5,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     config: {
     name: 'poll',
-    description: 'Creates poll',
+    description: 'Allows you to create poll',
     usage: `${prefix}poll`,
     category: 'util',
     access: 'everyone'
@@ -16,10 +16,10 @@ run: async (client, message, args) => {
     if (!args.join(' ')) return message.reply('Please input a question!')
 
     const embed = new MessageEmbed()
-    .setColor(colours.magenta)
+    .setColor(colours.blue)
     .setAuthor(`Poll created by ${message.author.tag}`, message.author.displayAvatarURL())
     .setDescription(args.join(' '))
-    .setFooter('React to vote.')
+    .setFooter('React to vote')
     .setTimestamp()
 
     message.delete()
